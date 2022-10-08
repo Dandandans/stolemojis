034c18ba58e6b4f650c336381cddfe59e8819fe2
Dan Cabrisas <danhcas@gmail.com>
2022-10-08 10:10:25 -0400

    Add

--- index.js
+++ index.js
@@ -19,11 +19,10 @@ fs.readdir(DIR, (err, files) => {
       preamble += `<img src="stolemoji/${file}" height="64" width="64" title="${file}" alt="${file}" hspace="10" />`;
     });
 
-  });
-  
   fs.writeFile('./README.md', preamble, (err) => {
     if (err) {
       console.error(err);
     }
     console.log('Let me tell you, champion, you did it.');
   });
+});
